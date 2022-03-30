f3bd5d2b2e0fe7646f547ba06a9d04c8a19736f2
marcos <kevera900@gmail.com>
2022-03-30 20:56:57 -0300

    js aula

---
+++ main.js
@@ -0,0 +1,68 @@
+/*var nome = "Kevin";
+var idade = 23;
+alert(nome + " tem " + idade + " anos");*/
+
+/*var lista = ["uva", "maca"];
+lista.push("abacate");
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" "));*/
+
+/*var fruta = [
+  { nome: "maça", cor: "vermelha" },
+  { nome: "uva", cor: "roxo" },
+];
+
+console.log(fruta);*/
+
+/*var idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+  alert("maior de idade");
+} else {
+  alert("menor de idade");
+}*/
+
+/*var count = 0;
+
+while (count <= 5) {
+  console.log(count);
+  count += 1;
+}*/
+
+/*for (var i = 0; i < 5; i++) {
+  console.log(i);
+}*/
+
+/*var d = new Date();
+console.log(d);*/
+
+/*function soma(n1, n2) {
+  return n1 + n2;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+
+function validaIdade(idade) {
+  var validar;
+  if (idade >= 18) {
+    validar = true;
+    return validar;
+  } else {
+    validar = false;
+    return validar;
+  }
+}*/
+
+/*function handleClick() {
+  document.getElementById("agradecimento").innerHTML =
+    "<b>Clique aqui para acessar o google</b>";
+}
+function redirecionar() {
+  //window.open("https://google.com");
+  window.location.href = "https://google.com";
+}*/
